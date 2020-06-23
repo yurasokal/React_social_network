69dea6240a0925f166cf14b01e29aba6a568b0c1
yurasokal <yurasokal@gmail.com>
2020-06-23 17:42:48 +0300

    25
    + method arr - map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,16 +16,15 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
-
-    let dialogsData = [
-    {id: 1, name: 'Olga Geirangerfjord'},
-    {id: 2, name: 'Nazar Trolltunga'},
-    {id: 3, name: 'Sergiy Kjerag'},
-    {id: 4, name: 'Yuriy Preikestolen'},
-    {id: 5, name: 'Roman Trollstigen'},
-    {id: 6, name: 'Ihor Lofoten'}
-]
-    let messagesData = [
+    let dialogs = [
+        {id: 1, name: 'Olga Geirangerfjord'},
+        {id: 2, name: 'Nazar Trolltunga'},
+        {id: 3, name: 'Sergiy Kjerag'},
+        {id: 4, name: 'Yuriy Preikestolen'},
+        {id: 5, name: 'Roman Trollstigen'},
+        {id: 6, name: 'Ihor Lofoten'}
+    ]
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How is your learning, buddy ?'},
         {id: 3, message: 'Not bat ! It\'s really interesting !'},
@@ -33,28 +32,31 @@ const Dialogs = (props) => {
         {id: 5, message: 'Yo, yo, yo!'},
         {id: 6, message: 'Yo'}
     ]
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = messages.map(m => <Message message={m.message}/>);
 
-
-return (
-    <div className={s.dialogs}>
-        <div className={s.dialogsItems}>
-            <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-            <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-            <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-            <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-            <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-            <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                {dialogsElements}
+                {/*<DialogItem name={dialogs[0].name} id={dialogs[0].id} />*/}
+                {/*<DialogItem name={dialogs[1].name} id={dialogs[1].id} />*/}
+                {/*<DialogItem name={dialogs[2].name} id={dialogs[2].id}/>*/}
+                {/*<DialogItem name={dialogs[3].name} id={dialogs[3].id}/>*/}
+                {/*<DialogItem name={dialogs[4].name} id={dialogs[4].id}/>*/}
+                {/*<DialogItem name={dialogs[5].name} id={dialogs[5].id}/>*/}
+            </div>
+            <div className={s.messages}>
+                {messagesElements}
+                {/*<Message message={messages[0].message}/>*/}
+                {/*<Message message={messages[1].message}/>*/}
+                {/*<Message message={messages[2].message}/>*/}
+                {/*<Message message={messages[3].message}/>*/}
+                {/*<Message message={messages[4].message}/>*/}
+                {/*<Message message={messages[5].message}/>*/}
+            </div>
         </div>
-        <div className={s.messages}>
-            <Message message={messagesData[0].message}/>
-            <Message message={messagesData[1].message}/>
-            <Message message={messagesData[2].message}/>
-            <Message message={messagesData[3].message}/>
-            <Message message={messagesData[4].message}/>
-            <Message message={messagesData[5].message}/>
-        </div>
-    </div>
-)
+    )
 }
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,10 +4,16 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
-    let postsData = [
+    let posts = [
         {id: 1, name: 'Hey, how are you?', likesCount: 12},
-        {id: 2, name: "It's my first post!", likesCount: 11}
+        {id: 2, name: "It's my first post!", likesCount: 11},
+        {id: 3, name: 'Bla bla', likesCount: 12},
+        {id: 4, name: "It's my not a first post!", likesCount: 11}
     ]
+
+    let postsElements =
+        posts.map (p => <Post message={p.name} likesCount={p.likesCount}/>);
+
 
     return (
         <div className={s.postsBlock}>
@@ -21,8 +27,7 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postsData[0].name} likesCount={postsData[0].likesCount}/>
-                <Post message={postsData[1].name} likesCount={postsData[1].likesCount}/>
+                {postsElements}
             </div>
         </div>
     );
