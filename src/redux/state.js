b57ba7981e1e2e78bc51888072fc9b76acd62e16
yurasokal <yurasokal@gmail.com>
2020-07-09 21:12:25 +0300

    38
    - dispatch, action

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,5 @@
 
-let store ={
+let store = {
     _state: {
         profilePage: {
             posts: [
@@ -29,34 +29,37 @@ let store ={
             ]
         },
         sideBar: {
-    
+
         },
     },
+    _callSubscriber() {
+        console.log('State changed');
+    },
+
     getState() {
         return this._state;
     },
-   _callSubscriber() {
-        console.log('State changed');
-    },
-    addPost () {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0
-        };
-    
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state);
-    },
-    updateNewPostText (newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
-    },
-    subscribe (observer) {
+    subscribe(observer) {
         this._callSubscriber = observer;
     },
-    
+
+    dispatch(action) { // {type: 'ADD-POST'}
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            };
+
+             this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        }
+    }
+
 }
 
 window.store = store;
