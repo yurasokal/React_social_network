74309b607bc0687ea91d864daaddf17c090831ba
yurasokal <yurasokal@gmail.com>
2020-07-10 12:50:59 +0300

    39
    - action creator, action type

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,7 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE = 'UPDATE-NEW-POST-TEXT';
+
+
 
 let store = {
     _state: {
@@ -44,7 +48,7 @@ let store = {
     },
 
     dispatch(action) { // {type: 'ADD-POST'}
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -54,12 +58,19 @@ let store = {
              this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
     }
+}
 
+export const addPostActionCreator = () => ({type: ADD_POST})
+
+export const updateNewPostTextActionCreator = (text) => {
+    return {
+        type: UPDATE, newText: text
+    }
 }
 
 window.store = store;
