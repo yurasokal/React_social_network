37c70c4796c7b7ebfc4909a4a0220bf9ab889dcf
yurasokal <yurasokal@gmail.com>
2020-07-15 10:55:51 +0300

    42
    - redux

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,17 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+let initialState = {
+    posts: [
+        { id: 1, message: 'Hey, how are you?', likesCount: 12 },
+        { id: 2, message: "It's my first post!", likesCount: 11 },
+        { id: 3, message: 'Bla bla', likesCount: 12 },
+        { id: 4, message: "It's my not a first post!", likesCount: 11 }
+    ],
+    newPostText: 'print text here'
+};
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
---
+++ src/redux/redux-store.js
@@ -0,0 +1,14 @@
+import profileReducer from "./profile-reducer";
+import sidebarReducer from "./sidebar-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import { createStore, combineReducers } from "redux";
+
+let reducers = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer
+});
+
+let store = createStore(reducers);
+
+export default store;
