ccff6840851449318808c216e93cd80588c7df67
yurasokal <yurasokal@gmail.com>
2020-08-13 13:50:06 +0300

    52
    side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,12 +4,14 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        
+    let getUsers = () => {
+    if (props.users.length === 0) 
+    {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-            props.setUsers(response.data.items);
-        });
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
 
         // props.setUsers([
         //     {
@@ -41,20 +43,21 @@ let Users = (props) => {
     }
 
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => { 
-                                props.unfollow(u.id) 
+                            ? <button onClick={() => {d
+                                props.unfollow(u.id)
                             }}>Unfollow</button>
-                            : <button onClick={() => { 
-                                props.follow(u.id) 
-                                }}>Follow</button>}
+                            : <button onClick={() => {
+                                props.follow(u.id)
+                            }}>Follow</button>}
                     </div>
                 </span>
                 <span>
