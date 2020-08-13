2ac095dffea1a88843e197549d3b1fc616c8b10b
yurasokal <yurasokal@gmail.com>
2020-08-13 14:42:48 +0300

    53
    класовий компонент (  extends React.Component )

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -12,6 +12,9 @@ const Nav = () => {
         <div className={`${s.item} ${s.active}`}>
           <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
         </div>
+        <div className={`${s.item} ${s.active}`}>
+          <NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
+        </div>
         <div className={s.item}>
           <NavLink to="/news" activeClassName={s.activeLink}>News</NavLink>
         </div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,60 +3,35 @@ import styles from './users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
-let Users = (props) => {
-    let getUsers = () => {
-    if (props.users.length === 0) 
-    {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items);
-            });
-        }
-
-        // props.setUsers([
-        //     {
-        //         id: 1, photoUrl: 'https://vignette.wikia.nocookie.net/jamesbond/images/d/dc/James_Bond_%28Pierce_Brosnan%29_-_Profile.jpg/revision/latest?cb=20130506224906',
-        //         followed: false,
-        //         fullName: 'Dima Matviyko',
-        //         status: 'Veterinarian',
-        //         location:
-        //             { city: 'Alborg', country: 'Denmark' }
-        //     },
-        //     {
-        //         id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTW7di2Ws8CNdXfLa-IpsJPnUp8g9cfGQkmxA&usqp=CAU',
-        //         followed: true,
-        //         fullName: 'Liybomur Dzhus',
-        //         status: 'Dentist',
-        //         location:
-        //             { city: 'Ivano - Frankivsk', country: 'Ukraine' }
-        //     },
-        //     {
-        //         id: 3, photoUrl: 'https://www.telegraph.co.uk/content/dam/beauty/Oct/Oct%2019/Halle-Berry-Die-Another-Day.jpg?imwidth=450',
-        //         followed: false,
-        //         fullName: 'Yulia Chabarenko',
-        //         status: 'Stydent',
-        //         location:
-        //             { city: 'Nurnberg', country: 'Germany' }
-        //     },
-        // ]
-        // )
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        alert('new object');
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                });
     }
 
+    getUsers = () => {
+    
+}
+
+render() {
     return <div>
-        <button onClick={getUsers}>Get Users</button>
         {
-            props.users.map(u => <div key={u.id}>
+            this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => {d
-                                props.unfollow(u.id)
+                            ? <button onClick={() => {
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
                     </div>
                 </span>
@@ -72,5 +47,7 @@ let Users = (props) => {
         }
     </div>
 }
+}
+
 
 export default Users;
