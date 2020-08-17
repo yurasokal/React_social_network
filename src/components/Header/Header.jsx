41ca56b329b1eb70fcbadebe7e8fcb90744c7b03
yurasokal <yurasokal@gmail.com>
2020-08-17 17:10:40 +0300

    60_1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className={s.header}>
             <img src='https://pbs.twimg.com/profile_images/1137096400863485952/YW6qjuI8_400x400.jpg' alt='Lviv logo' />
+
+            <div className={s.loginBlock}>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
         </header>
     );
 }
---
+++ src/redux/auth-reducer.js
@@ -0,0 +1,30 @@
+const SET_USER_DATA = 'SET_USER_DATA';
+const UNFOLLOW = 'UNFOLLOW';
+
+
+
+let initialState = {
+    id: null,
+    email: null,
+    login: null,
+    // isFetching: false
+};
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER_DATA:
+            return {
+                ...state,
+                ...action.data
+
+            }
+        
+        default:
+            return state;
+    }
+}
+
+export const SetUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: {userId, email, login} }) // action creator
+
+
+export default authReducer;
