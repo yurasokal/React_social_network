0506997a4a91a31b575b8ce83b97de1a183abcb0
yurasokal <yurasokal@gmail.com>
2020-09-04 14:35:35 +0300

    77_2

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -4,7 +4,10 @@ import styles from './FormsControls.module.css';
 export const Textarea = ({input, meta, ...props}) => {
     return (
         <div className={styles.formControl + " " + styles.error}>
+            <div>
             <textarea {...input} {...props}/>
+            </div>
+    <span>"Some error"</span>
         </div>
     )
 }
